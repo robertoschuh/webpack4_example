08df0802ddc5359ded3a796412963228c95ef350
R0b <robbyschuh@gmail.com>
2019-07-09 08:40:07 +0000

    fixed code issues

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,22 @@
 const webpack = require('webpack');
 const path = require('path');
-const argv = require('yargs').argv
+const argv = require('yargs').argv;
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
-const inProduction = (argv.mode === 'production')
-
-console.log('inProduction', inProduction);
+const inProduction = (argv.mode === 'production');
 
 module.exports = {
        
+    
+   // This line is not necessary at all.
     entry: {
         app: './src/main.js'  
     }, 
 
     output: {
         path: path.resolve(__dirname, './dist'),
+       // filename: '[name].[hash].js',
         filename: inProduction ? '[name].[hash].jss' : '[name].jss',
         chunkFilename: inProduction ? '[id].[hash].jss' : '[id].jss'
     }, 
@@ -50,7 +51,6 @@ module.exports = {
                             // by default it uses publicPath in webpackOptions.output
                             publicPath: './',
                             hmr: process.env.NODE_ENV ===  inProduction
-                         //   minimize: inProduction
                         //  reloadAll: true
 
                         },
